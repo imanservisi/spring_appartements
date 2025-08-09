6323c91bbddcc000fdd762e7480753af3dfa9386
imanservisi <popovicsisabelle@gmail.com>
2025-08-09 10:07:33 +0200

    création projet

---
+++ src/main/java/com/isabelle/demo/SpringAppartementsApplication.java
@@ -0,0 +1,13 @@
+package com.isabelle.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppartementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppartementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isabelle/demo/SpringAppartementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isabelle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppartementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
